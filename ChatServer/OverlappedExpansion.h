aa9a10082ed8a3c5132c6806935d6f0b6ade3d94
mgcllee <mgcllee@gmail.com>
2025-01-01 14:59:51 +0900

    [Server][update] OverlappedExpansion Class

--- ChatServer/OverlappedExpansion.h
+++ ChatServer/OverlappedExpansion.h
@@ -12,23 +12,9 @@ enum SOCKET_TYPE { ACCEPT, RECV, SEND, LOGOUT };
 
 class OverlappedExpansion {
 public:
-	OverlappedExpansion() { 
-		wsa_buffer.len = BUF_SIZE;
-		wsa_buffer.buf = packet_buffer;
-		socket_type = SOCKET_TYPE::RECV;
-		ZeroMemory(&overlapped, sizeof(overlapped));
-		remain_packet_size = 0;
-	}
-	OverlappedExpansion(char* packet) { 
-		socket_type = SOCKET_TYPE::RECV; 
-		wsa_buffer.len = packet[0];
-		wsa_buffer.buf = packet_buffer;
-		ZeroMemory(&overlapped, sizeof(overlapped));
-		socket_type = SEND;
-		memcpy(packet_buffer, packet, packet[0]);
-		remain_packet_size = 0;
-	}
-	~OverlappedExpansion() {};
+	OverlappedExpansion();
+	OverlappedExpansion(char* packet);
+	~OverlappedExpansion();
 
 public:
 	WSAOVERLAPPED overlapped;
