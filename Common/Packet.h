363369ca901939213177c9356476080ef662e063
mgcllee <mgcllee@gmail.com>
2025-01-05 17:30:46 +0900

    [Packet][new] C2S_LOGIN_PACK packet

--- Common/Packet.h
+++ Common/Packet.h
@@ -6,11 +6,21 @@
 #define MAX_BUF 100
 
 enum C2S_PACKET_TYPE {
-	SEND_CHAT_PACK = 1,
+	LOGIN_PACK = 1,
+	SEND_CHAT_PACK,
 	REQUEST_CHAT_LOG_PACK
 };
 
 #pragma pack (push, 2)
+
+struct C2S_LOGIN_PACK {
+	short size;
+	short type;
+
+	short length;
+	wchar_t id[MAX_BUF];
+	wchar_t pw[MAX_BUF];
+};
 
 struct C2S_SEND_CHAT_PACK {
 	short size;
