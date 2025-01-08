902e6ec5db7f66f3be41fba9a3e06f6c90e5d952
mgcllee <mgcllee@gmail.com>
2025-01-08 18:25:31 +0900

    [Packet][update] packet structure (use inheritance)

--- Common/Packet.h
+++ Common/Packet.h
@@ -8,31 +8,34 @@
 enum C2S_PACKET_TYPE {
 	LOGIN_PACK = 1,
 	SEND_CHAT_PACK,
-	REQUEST_CHAT_LOG_PACK
+	REQUEST_CHAT_LOG_PACK,
+	LOGOUT_PACK
 };
 
 #pragma pack (push, 2)
 
-struct C2S_LOGIN_PACK {
+struct BASIC_PACK {
 	short size;
 	short type;
+};
 
-	short length;
+struct C2S_LOGIN_PACK : BASIC_PACK {
 	wchar_t id[MAX_BUF];
 	wchar_t pw[MAX_BUF];
 };
 
-struct C2S_SEND_CHAT_PACK {
-	short size;
-	short type;
-	
+struct C2S_SEND_CHAT_PACK : BASIC_PACK {
 	short length;
 	wchar_t str[MAX_BUF];
 };
 
-struct C2S_REQUEST_CHAT_LOG_PACK {
-	short size;
-	short type;
+struct C2S_REQUEST_CHAT_LOG_PACK : BASIC_PACK {
+	
+};
+
+struct C2S_LOGOUT_PACK : BASIC_PACK {
+	wchar_t id[MAX_BUF];
+	wchar_t pw[MAX_BUF];
 };
 
 #pragma pack (pop)
@@ -47,17 +50,11 @@ enum S2C_PACKET_TYPE {
 
 #pragma pack (push, 2)
 
-struct S2C_LOGIN_RESULT_PACK {
-	short size;
-	short type;
-
+struct S2C_LOGIN_RESULT_PACK : BASIC_PACK {
 	wchar_t result[MAX_BUF];
 };
 
-struct S2C_SEND_CHAT_LOG_PACK {
-	short size;
-	short type;
-
+struct S2C_SEND_CHAT_LOG_PACK : BASIC_PACK {
 	char length;
 	wchar_t str[MAX_BUF];
 };
