dfcc0670096c0c4e3b4ef0927e1799cb11abb157
mgcllee <mgcllee@gmail.com>
2025-02-24 16:28:52 +0900

    [Server][update] interface method

--- ChatServer/public/Interface/IGrain.h
+++ ChatServer/public/Interface/IGrain.h
@@ -6,5 +6,22 @@
 class IGrain {
 public:
 	virtual void packet_worker(std::tuple<HANDLE, HANDLE, HANDLE, HANDLE> h_iocps) = 0;
+
+protected:
 	virtual bool is_exist_GQCS_result(OverlappedExpansion* exoverlapped, BOOL GQCS_result) = 0;
+	
+	void post_exoverlapped(
+		HANDLE h_iocp
+		, wchar_t packet_buffer[]
+		, std::wstring user_id
+		, OVERLAPPED_TYPE overlapped_type
+	) 
+	{
+		DWORD bytes = sizeof(OverlappedExpansion);
+		OverlappedExpansion* exoverlapped = new OverlappedExpansion();
+		exoverlapped->overlapped_type = overlapped_type;
+		wcscpy_s(exoverlapped->packet_buffer, packet_buffer);
+		exoverlapped->user_id = user_id;
+		PostQueuedCompletionStatus(h_iocp, bytes, 0, &exoverlapped->overlapped);
+	}
 };
